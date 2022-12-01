7d89868549a5cd5719d5a9e231430ef86c18f171
winfrednyoroka <winfredgatua13@gmail.com>
2022-12-01 14:09:35 +0000

    modified the mainapp.py to include creating the sql database and tables

--- mainapp.py
+++ mainapp.py
@@ -1,10 +1,20 @@
 import streamlit as st
 
-import sqlite3
+import sqlite3 as sql
 
+Mr_EvidenceDB =sql.connect("Mr_EvidenceDB")
+c=Mr_EvidenceDB.cursor()
+#create the tables if they do not exist
+c.execute("""CREATE TABLE IF NOT EXISTS "effectsizetype" ("id" TEXT,"effectsizetype" TEXT,PRIMARY KEY("id"));""")
+c.execute("""CREATE TABLE IF NOT EXISTS "exposure" ("exposureid"	TEXT,"exposurename"	TEXT,PRIMARY KEY("exposureid"));""")
+c.execute("""CREATE TABLE IF NOT EXISTS "methods" ("methodid" TEXT,"methodname" TEXT,PRIMARY KEY("methodid"));""")
+c.execute("""CREATE TABLE IF NOT EXISTS "outcome" ("outcomeid" TEXT,"outcomename" TEXT,PRIMARY KEY("outcomeid"));""")
+c.execute("""CREATE TABLE IF NOT EXISTS "study" ("pmid"	INTEGER,"title"	TEXT,"population" TEXT,"sex" TEXT,"mean_age" INTEGER,"median_age" INTEGER,"lower_age"INTEGER,"upper_age" INTEGER,"year" INTEGER,"samplesize" INTEGER,"author" TEXT,PRIMARY KEY("pmid"));""")
+c.execute("""CREATE TABLE IF NOT EXISTS "results" ("results_id" TEXT,"pmid" INTEGER,"methodid" TEXT,"effectsize" REAL,"lowerinterval"	REAL,"upperinterval" REAL,"pvalue" REAL,"exposureid" TEXT,"outcomeid" TEXT,"effectsizetype_id" TEXT,PRIMARY KEY("results_id"));""")
 def form():
     st.write("This is my data entry form")
     with st.form(key="Data enytry", clear_on_submit=True):
+        username=st.text_input("Enter your username")
         pmid=st.number_input("Enter the study pmid")
         title=st.text_input("Enter the study title")
         population=st.text_input("Enter the population/ancestry")
@@ -24,15 +34,37 @@ def form():
         methodid=st.number_input("Enter the unique ID of methods")
         id=st.number_input("Enter unique identifier for each of the effectsizetype")
         effectsizetype=st.text_input("Enter the effectsizetype")
+        resultsid=st.text_input("Enter the results id")
         effectsize=st.number_input("Enter the effcetsize")
         lowerinterval=st.number_input("Enter the lower CI")
         upperinterval=st.number_input("Enter the upper CI")
         pvalue=st.number_input("Enter the pvalue")
-
-        
-
-
         #image=st.camera_input("Take a picture to upload")
         submit_button=st.form_submit_button(label="SUBMIT")
+        if submit_button:
+            adddata(pmid,title,population,sex,mean_age,median_age,lower_age,upper_age,year,samplesize,author,exposurename,exposureid,outcomename,outcomeid,methodname,methodid,id,effectsizetype,resultsid,effectsize,lowerinterval,upperinterval,pvalue)
+            
+def adddata(pmid,title,population,sex,mean_age,median_age,lower_age,upper_age,year,samplesize,author,exposurename,exposureid,outcomename,outcomeid,methodname,methodid,id,effectsizetype,resultsid,effectsize,lowerinterval,upperinterval,pvalue):
+    """Populate the tables with data"""
+    #c.execute("""CREATE TABLE IF NOT EXISTS "effectsizetype" ("id" TEXT,"effectsizetype" TEXT,PRIMARY KEY("id"));""")
+    c.execute("INSERT INTO effectsizetype VALUES (?,?)",(id,effectsizetype))
+
+    #c.execute("""CREATE TABLE IF NOT EXISTS "exposure" ("exposureid"	TEXT,"exposurename"	TEXT,PRIMARY KEY("exposureid"));""")
+    c.execute("INSERT INTO exposure VALUES (?,?)",(exposureid,exposurename))
+
+    #c.execute("""CREATE TABLE IF NOT EXISTS "methods" ("methodid" TEXT,"methodname" TEXT,PRIMARY KEY("methodid"));""")
+    c.execute("INSERT INTO methods VALUES (?,?)",(methodid,methodname))
+
+    #c.execute("""CREATE TABLE IF NOT EXISTS "outcome" ("outcomeid" TEXT,"outcomename" TEXT,PRIMARY KEY("outcomeid"));""")
+    c.execute("INSERT INTO outcome VALUES (?,?)",(outcomeid,outcomename))
+
+    #c.execute("""CREATE TABLE IF NOT EXISTS "study" ("pmid"	INTEGER,"title"	TEXT,"population" TEXT,"sex" TEXT,"mean_age" INTEGER,"median_age" INTEGER,"lower_age"INTEGER,"upper_age" INTEGER,"year" INTEGER,"samplesize" INTEGER,"author" TEXT,PRIMARY KEY("pmid"));""")
+    c.execute("INSERT INTO study VALUES (?,?,?,?,?,?,?,?,?,?,?)",(pmid,title,population,sex,mean_age,median_age,lower_age,upper_age,year,samplesize,author))
+
+    #c.execute("""CREATE TABLE IF NOT EXISTS "results" ("results_id" TEXT,"pmid" INTEGER,"methodid" TEXT,"effectsize" REAL,"lowerinterval"	REAL,"upperinterval" REAL,"pvalue" REAL,"exposureid" TEXT,"outcomeid" TEXT,"effectsizetype_id" TEXT,PRIMARY KEY("results_id"));""")
+    c.execute("INSERT INTO results VALUES (?,?,?,?,?,?,?,?,?,?)",(results_id,pmid,methodid,effectsize,lowerinterval,upperinterval,pvalue,exposureid,outcomeid,effectsizetype_id))
+    Mr_EvidenceDB.commit()
+    Mr_EvidenceDB.close()
+    st.success("Hello {} you have entered the data for this study PMID {} ".format(username,pmid))
 
 form()
