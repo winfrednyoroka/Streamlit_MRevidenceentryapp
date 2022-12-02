096372464d9e960d1a98423ec3a164d8771c970d
winfrednyoroka <winfredgatua13@gmail.com>
2022-12-02 14:54:41 +0000

    modified the mainapp.py to include stratified results reporting

--- mainapp.py
+++ mainapp.py
@@ -47,9 +47,9 @@ def form():
             outentry(outcomename,outcomeid)
     with st.form(key="METHOD",clear_on_submit=True):
         st.subheader("METHOD TABLE")
-        st.write("M1:IVW,M2:Wetmedian,M3:Wetmode,M4:MREgger,M5:IVestimator,M6:MVMR,M7:penalisedwetmedian")
-        methodname=st.selectbox("Enter the method name",["IVW","Wetmedian","Wetmode","MREgger","IVestimator","MVMR","penalisedwetmedian"])
-        methodid=st.selectbox("Enter the unique ID of methods",["M1","M2","M3","M4","M5","M6","M7"])
+        st.write("M1:IVW,M2:Wetmedian,M3:Wetmode,M4:MREgger,M5:IVestimator,M6:MVMR,M7:penalisedwetmedian,M8:SIMEXcorrectedMREgger,M9:MRGXE")
+        methodname=st.selectbox("Enter the method name",["IVW","Wetmedian","Wetmode","MREgger","IVestimator","MVMR","penalisedwetmedian","SIMEXcorrectedMREgger","MRGXE"])
+        methodid=st.selectbox("Enter the unique ID of methods",["M1","M2","M3","M4","M5","M6","M7","M8","M9"])
         submit_methodbutton=st.form_submit_button(label="Submitmethodentry")
         if submit_methodbutton:
             methodentry(methodname,methodid)
@@ -64,8 +64,8 @@ def form():
     with st.form(key="RESULTS",clear_on_submit=True):
         st.subheader("RESULTS TABLE")
         pmid=st.number_input("Enter the study pmid",value=0)
-        st.write("M1:IVW,M2:Wetmedian,M3:Wetmode,M4:MREgger,M5:IVestimator,M6:MVMR,M7:penalisedwetmedian")
-        methodid=st.selectbox("Enter the unique ID of methods",["M1","M2","M3","M4","M5","M6","M7"])
+        st.write("M1:IVW,M2:Wetmedian,M3:Wetmode,M4:MREgger,M5:IVestimator,M6:MVMR,M7:penalisedwetmedian,M8:SIMEXcorrectedMREgger,M9:MRGXE")
+        methodid=st.selectbox("Enter the unique ID of methods",["M1","M2","M3","M4","M5","M6","M7","M8","M9"])
         st.write("E1:BMI,E2:Adiposity,E3:WHR,E4:childhoodBMI,E5:adultBMI,E6:fatmassindex,E7:fatfreemassindex,E8:FA,E9:UFA,E10:obesity,E11:bodyfat%,E12:BMIWGRS,E13:BMIGRS")
         exposureid=st.selectbox("Enter expid",["E1","E2","E3","E4","E5","E6","E7","E8","E9","E10","E11","E12","E13"])
         st.write("O1:incident hypertension,O2:ever hypertension,O3:hypertension,O4:gestational hypertension,O5:essential hypertension,O6:SBP,O7:DBP,O8:PAP,O9:PVremodelling,O10:Grade1diastolicdysfunction,O11:arterial hypertension")
@@ -77,10 +77,12 @@ def form():
         lowerinterval=st.number_input("Enter the lower CI",format="%.6f")
         upperinterval=st.number_input("Enter the upper CI",format="%.6f")
         pvalue=st.number_input("Enter the pvalue",format="%.200f")
+        se=st.number_input("Enter the standard error", format="%.6f")
+        strata=st.text_input("Enter the stratification of results")
         #image=st.camera_input("Take a picture to upload")
         submit_button=st.form_submit_button(label="SUBMIT")
         if submit_button:
-            resultsentry(resultsid,pmid,methodid,effectsize,lowerinterval,upperinterval,pvalue,exposureid,outcomeid,effectsizetype_id)
+            resultsentry(resultsid,pmid,methodid,effectsize,lowerinterval,upperinterval,pvalue,exposureid,outcomeid,effectsizetype_id,se)
             #adddata(pmid,title,population,sex,mean_age,median_age,lower_age,upper_age,year,samplesize,author)#exposurename,exposureid,outcomename,outcomeid,methodname,methodid,id,effectsizetype,resultsid,effectsize,lowerinterval,upperinterval,pvalue)
 def expentry(a,b):
 	c.execute("INSERT INTO exposure VALUES (?,?)",(a,b))
@@ -92,8 +94,8 @@ def outentry(a,b):
 	Mr_EvidenceDB.commit()
     #Mr_EvidenceDB.close()
     
-def resultsentry(a,b,d,e,f,g,h,i,j,k):
-	c.execute("INSERT INTO results VALUES (?,?,?,?,?,?,?,?,?,?)",(a,b,d,e,f,g,h,i,j,k))
+def resultsentry(a,b,d,e,f,g,h,i,j,k,l,m):
+	c.execute("INSERT INTO results VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",(a,b,d,e,f,g,h,i,j,k,l,m))
 	Mr_EvidenceDB.commit()
     #st.success("HI! you have entered the Results for this results_id {}".format(resultsid))
     #Mr_EvidenceDB.close()
