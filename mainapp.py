925fcd423876ad55474cd1cf2c03abd782e5bf5c
winfrednyoroka <winfredgatua13@gmail.com>
2022-12-02 13:13:36 +0000

    modified the script for numbers to certain decimal places

--- mainapp.py
+++ mainapp.py
@@ -15,7 +15,7 @@ def form():
     with st.form(key="STUDY", clear_on_submit=True):
         st.subheader("STUDY TABLE")
         username=st.selectbox("Enter your username",["Winfred","Tom","Yi","Debbie","Chris"])
-        pmid=st.number_input("Enter the study pmid")
+        pmid=st.number_input("Enter the study pmid",value=0)
         title=st.text_input("Enter the study title")
         population=st.selectbox("Enter the population/ancestry",["EUR","East Asian","Korean","AFR","AFR_AMR"])
         sex=st.selectbox("sex of participants",["female","male","both"])
@@ -23,8 +23,8 @@ def form():
         median_age=st.number_input("Enter the median_age of the participants")
         lower_age=st.number_input("Enter the lower_age of the participants")
         upper_age=st.number_input("Enter the upper_age of the participants")
-        year=st.number_input("Enter the year of pulication")
-        samplesize=st.number_input("Enter the total sample size of the population under investigation")
+        year=st.number_input("Enter the year of pulication",value=0)
+        samplesize=st.number_input("Enter the total sample size of the population under investigation",value=0)
         author=st.text_input("Enter the author names")
         submit_studybutton=st.form_submit_button(label="Submitstudyentry")
         if submit_studybutton:
@@ -39,9 +39,9 @@ def form():
             expentry(exposurename,exposureid)
     with st.form(key="OUTCOME",clear_on_submit=True):
         st.subheader("OUTCOME TABLE")
-        st.write("O1:incident hypertension,O2:ever hypertension,O3:hypertension,O4:gestational hypertension,O5:essential hypertension,O6:SBP,O7:DBP,O8:PAP,O9:PVremodelling,O10:Grade1diastolicdysfunction")
-        outcomename=st.selectbox("Enter the name of the outcome",["incident hypertension","ever hypertension","hypertension","gestational hypertension","essential hypertension","SBP","DBP","PAP","PVremodelling","Grade1diastolicdysfunction"])
-        outcomeid=st.selectbox("Enter the unique outcome identifier",["O1","O2","O3","O4","O5","O6","O7","O8","O9","O10"])
+        st.write("O1:incident hypertension,O2:ever hypertension,O3:hypertension,O4:gestational hypertension,O5:essential hypertension,O6:SBP,O7:DBP,O8:PAP,O9:PVremodelling,O10:Grade1diastolicdysfunction,O11:arterialhypertension")
+        outcomename=st.selectbox("Enter the name of the outcome",["incident hypertension","ever hypertension","hypertension","gestational hypertension","essential hypertension","SBP","DBP","PAP","PVremodelling","Grade1diastolicdysfunction","arterial hypertension"])
+        outcomeid=st.selectbox("Enter the unique outcome identifier",["O1","O2","O3","O4","O5","O6","O7","O8","O9","O10","O11"])
         submit_outcomebutton=st.form_submit_button(label="Submitoutcomeentry")
         if submit_outcomebutton:
             outentry(outcomename,outcomeid)
@@ -55,28 +55,28 @@ def form():
             methodentry(methodname,methodid)
     with st.form(key="EFFECTSIZE",clear_on_submit=True):
         st.subheader("EFFECTSIZETYPE TABLE")
-        st.write("ID1:OR,ID2:MD,ID3:HR")
-        id=st.selectbox("Enter unique identifier for each of the effectsizetype",["ID1","ID2","ID3"])
-        effectsizetype=st.selectbox("Enter the effectsizetype",["OR","MD","HR"])
+        st.write("ID1:OR,ID2:MD,ID3:HR,ID4:BETA")
+        id=st.selectbox("Enter unique identifier for each of the effectsizetype",["ID1","ID2","ID3","ID4"])
+        effectsizetype=st.selectbox("Enter the effectsizetype",["OR","MD","HR","BETA"])
         submit_effectsizebutton=st.form_submit_button(label="Submiteffectsizeentry")
         if submit_effectsizebutton:
             effectsizeentry(id,effectsizetype)
     with st.form(key="RESULTS",clear_on_submit=True):
         st.subheader("RESULTS TABLE")
-        pmid=st.number_input("Enter the study pmid")
+        pmid=st.number_input("Enter the study pmid",value=0)
         st.write("M1:IVW,M2:Wetmedian,M3:Wetmode,M4:MREgger,M5:IVestimator,M6:MVMR,M7:penalisedwetmedian")
         methodid=st.selectbox("Enter the unique ID of methods",["M1","M2","M3","M4","M5","M6","M7"])
         st.write("E1:BMI,E2:Adiposity,E3:WHR,E4:childhoodBMI,E5:adultBMI,E6:fatmassindex,E7:fatfreemassindex,E8:FA,E9:UFA,E10:obesity,E11:bodyfat%,E12:BMIWGRS,E13:BMIGRS")
         exposureid=st.selectbox("Enter expid",["E1","E2","E3","E4","E5","E6","E7","E8","E9","E10","E11","E12","E13"])
-        st.write("O1:incident hypertension,O2:ever hypertension,O3:hypertension,O4:gestational hypertension,O5:essential hypertension,O6:SBP,O7:DBP,O8:PAP,O9:PVremodelling,O10:Grade1diastolicdysfunction")
-        outcomeid=st.selectbox("Enter the unique outcome identifier",["O1","O2","O3","O4","O5","O6","O7","O8","O9","O10"])
+        st.write("O1:incident hypertension,O2:ever hypertension,O3:hypertension,O4:gestational hypertension,O5:essential hypertension,O6:SBP,O7:DBP,O8:PAP,O9:PVremodelling,O10:Grade1diastolicdysfunction,O11:arterial hypertension")
+        outcomeid=st.selectbox("Enter the unique outcome identifier",["O1","O2","O3","O4","O5","O6","O7","O8","O9","O10","O11"])
         resultsid=st.text_input("Enter the results id")
-        effectsize=st.number_input("Enter the effectsize")
-        st.write("ID1:OR,ID2:MD,ID3:HR")
-        effectsizetype_id=st.selectbox("Enter unique identifier for each of the effectsizetype",["ID1","ID2","ID3"])
-        lowerinterval=st.number_input("Enter the lower CI")
-        upperinterval=st.number_input("Enter the upper CI")
-        pvalue=st.number_input("Enter the pvalue")
+        effectsize=st.number_input("Enter the effectsize",format="%.6f")
+        st.write("ID1:OR,ID2:MD,ID3:HR,ID4:BETA")
+        effectsizetype_id=st.selectbox("Enter unique identifier for each of the effectsizetype",["ID1","ID2","ID3","ID4"])
+        lowerinterval=st.number_input("Enter the lower CI",format="%.6f")
+        upperinterval=st.number_input("Enter the upper CI",format="%.6f")
+        pvalue=st.number_input("Enter the pvalue",format="%.200f")
         #image=st.camera_input("Take a picture to upload")
         submit_button=st.form_submit_button(label="SUBMIT")
         if submit_button:
@@ -95,6 +95,7 @@ def outentry(a,b):
 def resultsentry(a,b,d,e,f,g,h,i,j,k):
 	c.execute("INSERT INTO results VALUES (?,?,?,?,?,?,?,?,?,?)",(a,b,d,e,f,g,h,i,j,k))
 	Mr_EvidenceDB.commit()
+    #st.success("HI! you have entered the Results for this results_id {}".format(resultsid))
     #Mr_EvidenceDB.close()
 
 def methodentry(a,b):
